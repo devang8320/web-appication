fa52b35ebfd50b1668d9de2f0f177deeada47239
= <=>
2025-01-05 13:58:51 -0500

    input box added ( forward reference)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState ,useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import authService  from './appwrite/auth'
 import { login,logout } from './store/authSlice'
-import { Header, Footer } from './components'
+import { Header,Footer } from './components'
 
 
 
