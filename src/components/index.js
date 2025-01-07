61e049dd5f80cece51e95fcc92aaf2421ca7e826
= <=>
2025-01-07 13:32:40 -0500

    Login and Signup added

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,11 @@ import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import Input from "./input"
+import Select from "./Select"
+import PostCard from "./PostCard"
+import Button from "./Button"
+import Login from "./Login"
+import Signup from "./SignUp"
 
-export {Header, Footer, Container,Logo, LogoutBtn}
+export {Header, Footer, Container,Logo, LogoutBtn, Input, Select, PostCard, Button, Login, Signup}
